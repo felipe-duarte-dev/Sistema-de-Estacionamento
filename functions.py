fafb5e32633ba06fa35cc805285659e8b727c935
Felipe Duarte <felipeaugustoduarte2@gmail.com>
2025-07-02 00:25:02 -0300

    Projeto base

---
+++ functions.py
@@ -0,0 +1,76 @@
+# Função para estacionar um carro em uma vaga disponível dos níveis existentes
+def estacionar_carro(dicionario_niveis, niveis_estacionamento):
+    #Verifica se o nível é um número inteiro
+    while True:
+        try:
+            nivel_estacionar = input(f"Digite o nível para estacionar (1 a {niveis_estacionamento}) (ou 0 para cancelar): ") # Recebe o nível que o usuário deseja estacionar
+            nivel_estacionar = int(nivel_estacionar) 
+
+            # Caso o nível não esteja entre os níveis existentes o código executa novamente o loop
+            if nivel_estacionar not in dicionario_niveis:
+                print("Nível inexistente!")
+                continue
+            # Cancela a operação
+            elif nivel_estacionar == 0:
+                print("Operação cancelada!")
+                return dicionario_niveis
+            
+            break
+        except ValueError:
+            print("Nível inválido!")
+
+    #Verifica se a vaga escolhida é um número inteiro
+    while True:
+        try:
+            print(f"Vagas disponíveis no nível {nivel_estacionar}: {dicionario_niveis[nivel_estacionar]}") # Exibe as vagas disponíveis para estacionar no nível escolhido
+            vaga_estacionar = input("Digite o número da vaga (ou 0 para cancelar): ") # Recebe a vaga que o usuário deseja estacionar
+            vaga_estacionar = int(vaga_estacionar)
+
+            # Cancela a operação
+            if vaga_estacionar == 0:
+                print("Operação cancelada!")
+                return dicionario_niveis
+            
+            dicionario_niveis[nivel_estacionar].remove(vaga_estacionar) # Atualiza o dicionário, retirando a vaga ocupada
+            print("Operação realizada com sucesso!") 
+            return dicionario_niveis # Encerra a operação e retorna o dicionário atualizado com a vaga preenchida
+        except ValueError:
+            print("Vaga inexistente!")
+
+def retirar_carro(dicionario_niveis):
+    # Verifica se o nível é um número inteiro
+    while True:
+        try:
+            nivel_retirar = input("Digite o nível do carro (ou 0 para cancelar): ") # Recebe o nível que o usuário deseja fazer a retirada
+            nivel_retirar = int(nivel_retirar)
+
+            if nivel_retirar not in dicionario_niveis: # Verifica se o nível faz parte dos níveis existentes
+               print("Nível inexistente!")
+               continue
+            elif nivel_retirar == 0: # Cancela a operação
+                print("Operação cancelada!")
+                return dicionario_niveis
+            
+            break
+        except ValueError:
+            print("Nível inválido!")
+    
+    #Verifica se o número da vaga é um número inteiro
+    while True:
+        try:
+            vaga_retirar = input("Digite o número da vaga (ou 0 para cancelar): ") # Recebe a vaga que o carro está estacionado
+            vaga_retirar = int(vaga_retirar)
+
+            if vaga_retirar in dicionario_niveis[nivel_retirar]: # Verifica se a vaga está no dicionário, caso ela esteja significa que a vaga não está ocupada ou não existe
+                print(f"A vaga {vaga_retirar} não está ocupada ou não existe")
+                continue # Reinicia o loop até o usuário digitar uma vaga ocupada
+            elif vaga_retirar == 0: # Cancela a operação
+                print("Operação cancelada!")
+                return dicionario_niveis
+
+            dicionario_niveis[nivel_retirar].append(vaga_retirar) # Adiciona a vaga novamente ao dicionário, listando ela como disponível
+            print("Você ficou no estacionamento entre 20h às 21h \nOperação realizada com sucesso!")
+            return dicionario_niveis # Encerra a operação e retorna o dicionário atualizado
+            
+        except ValueError:
+            print("Vaga inexistente!")
---
+++ main.py
@@ -0,0 +1,51 @@
+from functions import estacionar_carro, retirar_carro # Importa as funções do arquivo functions
+
+print("=== Sistema de Estacionamento XXXX Shopping ===")
+
+while True:
+    # Verifica se a entrada do usuário é um número inteiro
+    try:
+        niveis_estacionamento = input("Quantos níveis o estacionamento possui? (mínimo 1): ") # A variável niveis_estacionamento recebe uma string e em seguida converte para um inteiro
+        niveis_estacionamento = int(niveis_estacionamento)
+        break
+    except ValueError:
+        print("Número de níveis inválido!")
+
+while True:
+    # Verifica se a entrada do usuário é um número inteiro
+    try:
+        vagas_nivel = input("Quantas vagas por nível? ") # A variável vagas_nivel recebe uma string e em seguida converte para um inteiro
+        vagas_nivel = int(vagas_nivel)
+        break
+    except ValueError:
+        print("Número de vagas inválido!")
+
+
+dicionario_niveis = {} # Cria um dicionário para guardar o níveis (chaves) e os números de vagas (valores)
+nomes_niveis = [niveis for niveis in range(1, niveis_estacionamento + 1)] # Cria uma lista para armazenar as chaves do dicionário
+
+for nivel in nomes_niveis:
+    dicionario_niveis[nivel] = [vagas for vagas in range(1, vagas_nivel + 1)] # Adiciona o número de vagas por nível a cada nível {nível: [vagas]}
+
+print("Opções \n1 - Estacionar carro \n2 - Sair com carro \n3 - Encerrar sistema") # Mostra as opções de funções ao usuário
+
+while True:
+    # Verifica se a opção é um valor inteiro
+    try:
+        opcao = input("Escolha uma opção: ")
+        opcao = int(opcao)
+        # Verifica se é uma opção válida dentre as opções existentes
+        if opcao not in [1, 2, 3]:
+            print("Opção Inválida")
+    except ValueError:
+        print("Opção Inválida")
+
+    # Executa a função associada a opção escolhida pelo usuário
+    match opcao:
+        case 1:
+            estacionar_carro(dicionario_niveis, niveis_estacionamento)
+        case 2:
+            retirar_carro(dicionario_niveis)  
+        case 3:
+            print("Encerrando o sistema...")
+            break
